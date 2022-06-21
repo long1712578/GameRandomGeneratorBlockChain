072ea642b99fd194864f01ba8a0994ab7dd9ac02
long1712578 <pdlong578@gmail.com>
2022-06-21 07:56:40 +0700

    run truffle compile

--- contracts/RandomNumber.sol
+++ contracts/RandomNumber.sol
@@ -2,6 +2,12 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract RandomNumber {
+
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
     function rand(uint seed) internal pure returns(uint) {
         bytes32 data;
         uint sum;
