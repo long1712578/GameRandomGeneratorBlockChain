265ee8965cd0c49f1cc2cc40fce5ca8c858867d2
long1712578 <pdlong578@gmail.com>
2022-06-21 07:24:35 +0700

    function random

---
+++ contracts/RandomNumber.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract RandomNumber {
+    function rand(uint seed) internal pure returns(uint) {
+        bytes32 data;
+        uint sum;
+
+        if(seed % 2 == 0){
+            data = keccak256(abi.encodePacked(seed));
+        }else{
+            data = keccak256(abi.encodePacked(keccak256(abi.encodePacked(seed))));
+        }
+
+        for(uint i = 0; i < 32; i++) {
+            sum += uint(uint8(data[i]));
+        }
+
+        return uint(uint8(data[sum % data.length])) + uint(uint8(data[(sum + 2) % data.length]));
+    }
+}
